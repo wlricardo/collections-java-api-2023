1e9a8cc39b46bc4abe1303bf413bb4ce2fe72e57
wlricardo <wlricardo@gmail.com>
2024-06-09 15:15:44 -0300

    Criando classes Item e CarrinhoDeCompras

---
+++ Entities/CarrinhoDeCompras.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+    List<Item> listaDeItens;
+
+    public CarrinhoDeCompras() {
+        listaDeItens = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade) {
+        listaDeItens.add(new Item(nome, preco, quantidade));
+    }
+
+    public void removerItem(String nome) {
+        List<Item> itensParaRemover = new ArrayList<>();
+        if (!listaDeItens.isEmpty()) {
+            for (Item item : listaDeItens) {
+                if (item.getNome().equals(nome)) {
+                    itensParaRemover.add(item);
+                }
+            }
+            listaDeItens.removeAll(itensParaRemover);
+        } else {
+            System.out.println("\n A lista de itens está vazia.");
+        }
+    }
+
+    public double calcularValorTotal() {
+        double total = 0.0;
+        if (!listaDeItens.isEmpty()) {
+            for (Item item : listaDeItens) {
+                total += item.getPreco();
+            }
+        } else {
+            System.out.println("\n A lista de itens está vazia.");
+        }
+        return total;
+    }
+
+    public void exibirItens() {
+        if (!listaDeItens.isEmpty()) {
+            for (Item item : listaDeItens) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("\n A lista de itens está vazia.");
+        }
+    }
+
+}
--- Entities/Item.java
+++ Entities/Item.java
@@ -1,13 +1,13 @@
 public class Item {
     private String nome;
-    private String preco;
+    private double preco;
     private int quantidade;
 
     public Item() {
         super();
     }
 
-    public Item(String nome, String preco, int quantidade) {
+    public Item(String nome, double preco, int quantidade) {
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
@@ -21,11 +21,11 @@ public class Item {
         this.nome = nome;
     }
 
-    public String getPreco() {
+    public double getPreco() {
         return preco;
     }
 
-    public void setPreco(String preco) {
+    public void setPreco(double preco) {
         this.preco = preco;
     }
 
@@ -35,5 +35,12 @@ public class Item {
 
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "\nItem : " + nome 
+        + "\n Preço : R$ %.2f" + preco 
+        + "\n Quantidade : " + quantidade + "\n";
     }    
 }
