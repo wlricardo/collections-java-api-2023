c1d3ee93ff0fdfa2d58e9240d9cf31e5eb8f6004
wlricardo <wlricardo@gmail.com>
2024-06-09 14:43:41 -0300

    Criando classe Item

---
+++ Entities/Item.java
@@ -0,0 +1,39 @@
+public class Item {
+    private String nome;
+    private String preco;
+    private int quantidade;
+
+    public Item() {
+        super();
+    }
+
+    public Item(String nome, String preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }    
+}
